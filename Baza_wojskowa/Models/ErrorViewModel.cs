85299c6df86bae26d999281b6f65d1abdd6a9868
Mikolaj <Mikolaj@DESKTOP-MMEO5AD>
2024-12-30 18:35:24 +0100

    Dodaj pliki projektów.

---
+++ Baza_wojskowa/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Baza_wojskowa.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
