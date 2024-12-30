d51000df2ef237177bcab3e2ed42667dc118ca86
MikTopolsky <mikolaj.topolski@icloud.com>
2024-12-30 20:10:28 +0100

    xd

---
+++ Baza_wojskowa/Models/FilmDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Baza_wojskowa.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Filmy.Models
+{
+    public class FilmsDbContext : DbContext
+    {
+        public FilmsDbContext(DbContextOptions<FilmsDbContext> options) :
+        base(options)
+        {
+        }
+        public DbSet<Film> Films { get; set; }
+    }
+}
